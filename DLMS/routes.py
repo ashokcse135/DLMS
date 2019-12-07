27c26c5d6a285bf033f4f9a3364c6db992f6cbfa
Gunturu Ashok <49070724+ashokcse135@users.noreply.github.com>
2019-12-07 22:51:03 +0530

    Admin module is added.
    Add and Remove librarian is implemented by giving access to the admin on Librarian tables.

--- DLMS/routes.py
+++ DLMS/routes.py
@@ -1,4 +1,10 @@
 from flask import render_template, url_for, flash, redirect
 from DLMS import app
-#from flaskblog.forms import RegistrationForm, LoginForm
-#from flaskblog.models import User, Post
+from DLMS.models import *
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+admin=Admin(app)
+admin.add_view(ModelView(Librarian_t1,db.session))
+admin.add_views(ModelView(Librarian_t2,db.session))
