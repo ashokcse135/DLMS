618383ba682c962991f2026bab3070c9fb46d999
Gunturu Ashok <49070724+ashokcse135@users.noreply.github.com>
2019-12-07 22:47:25 +0530

    Secret is configured.

--- DLMS/__init__.py
+++ DLMS/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SECRET_KEY']=b'^$z\xf7\xcd\x96\xb8\xeb)\x02\x8d\x7f\xe4[*Fp\x0c5\x9aU\x96W('
 db=SQLAlchemy(app)
 
 from DLMS import  routes
